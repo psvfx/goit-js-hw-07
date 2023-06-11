d41d968b0474d7ec21f64576c83e677f62cf867d
psvfx <psvfxinvest@gmail.com>
2023-06-11 14:01:24 +0300

    added solutions to tasks 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+// console.log(galleryEl);
+
+const markupOfElementsGallery = galleryItems.map(
+  (galleryItems) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href=${galleryItems.original}>
+    <img
+      class="gallery__image "
+      src=${galleryItems.preview}
+      data-source=${galleryItems.original}
+      alt=${galleryItems.description}
+    />
+  </a>
+</li>`
+);
+
+// console.log(markupOfElementsGallery);
+
+galleryEl.insertAdjacentHTML("beforeend", markupOfElementsGallery.join(""));
+
+galleryEl.addEventListener("click", (el) => {
+  el.preventDefault();
+
+  const { target } = el;
+
+  if (target.dataset.source) {
+    console.log("click");
+  }
+
+  const modalGallery = `<img
+        width="100%"
+        height="100%"
+        src = ${el.target.dataset.source}
+      >`;
+
+  let modalImg;
+
+  const onEscKeyPress = (e) => {
+    if (e.key === "Escape") modalImg.close();
+  };
+  modalImg = basicLightbox.create(modalGallery, {
+    onShow: () => galleryEl.addEventListener("keydown", onEscKeyPress),
+  });
+  modalImg.show();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+// console.log(galleryEl);
+
+const markupOfElementsGallery = galleryItems.map(
+  (galleryItems) =>
+    `<li class="gallery__item">
+   <a class="gallery__link" href=${galleryItems.original}>
+      <img class="gallery__image" src=${galleryItems.preview} alt=${galleryItems.description} />
+   </a>
+</li>`
+);
+
+// console.log(markupOfElementsGallery);
+
+galleryEl.insertAdjacentHTML("beforeend", markupOfElementsGallery.join(""));
+
+galleryEl.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+}
+
+new SimpleLightbox(".gallery a", { captionDelay: 250, captionsData: "alt" });
